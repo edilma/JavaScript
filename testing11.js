1c07698c2f79fafa8de76e9a70b6cfa815a17f38
Edilma Riano <edilmaria@hotmail.com>
2020-04-29 07:52:52 -0400

    Chapter 11

--- testing11.js
+++ testing11.js
@@ -107,9 +107,89 @@
  
 //  console.log(getValidInput('Create a password:', isValidPassword));
 
-function callMe(func) {
-    func();
- }
+// function callMe(func) {
+//     func();
+//  }
  
- callMe("Al");
+//  callMe("Al");
 
+///***EXERCISER  */
+/// 11.10.1----
+// let answer = function(item){
+//     if (typeof item==="number") {
+//        return item*3;
+//     } else if (typeof item==="string") {
+//         return "ARRR!"
+//     } else {
+//         return item;
+//     }
+// };
+
+// let item = [2,5];
+// //console.log(answer(item));
+
+// /// 11.10.2----
+// let item2 =  ['Elocution', 21, 'Clean teeth', 100] ;
+// let mapArray = item2.map(answer)
+
+// console.log(mapArray);
+
+
+//**10-10-2 */
+
+let level2=function(level){
+    if (level > 100000) {
+        return level-100001
+    } else if (level>50000) {
+        return level-50001
+    } else
+    return 1;
+}
+
+let cargoMold = function(arr){
+    let arr2 =[];
+    let iGd =arr.indexOf('gold');
+    let iSt = arr.indexOf('satellite');
+    arr2.push(arr.splice(iGd,1,"water"));
+    arr2.push(arr.splice(iSt,1,"water"));
+    return arr2;
+
+};
+
+function irs(fuelLevel, cargoHold){
+    let lostFuel = level2 (fuelLevel);
+    let lostCargo = cargoMold(cargoHold);
+    return `Raided ${lostFuel} kg of fuel from the tanks, and stole ${lostCargo[0]} and ${lostCargo[1]} from the cargo hold.`
+}
+
+function checkFuel(level) {
+if (level > 100000){
+    level2(level)
+    return 'green';
+ } else if (level > 50000){
+    return 'yellow';
+ } else {
+    return 'red';
+ }
+};
+
+
+function holdStatus(arr){
+ if (arr.length < 7) {
+    return `Spaces available: ${7 - arr.length}`;
+ } else if (arr.length > 7){
+    return `Over capacity by ${arr.length - 7} items.`
+ } else {
+    return "Full";
+ }
+}
+
+let fuelLevel = 200000;
+let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold', 'water', 'AE-35 unit'];
+
+console.log("Fuel level: "+ checkFuel(fuelLevel));
+console.log("Hold status: "+ holdStatus(cargoHold));
+console.log( "the stolen fuel was: " + level2(fuelLevel));
+//console.log("The stolen cargo hold items are: " + cargoMold(cargoHold));
+//console.log ("Final Items in cargo hold are: " + cargoHold);
+console.log(irs(fuelLevel,cargoHold));
